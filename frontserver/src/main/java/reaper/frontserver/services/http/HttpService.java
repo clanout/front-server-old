460a39a370618903824e4086402ac4e40c1710a1
Aditya Prasad <aytida77@gmail.com>
2015-09-03 19:09:16 +0530

    Http status code fix

--- frontserver/src/main/java/reaper/frontserver/services/http/HttpService.java
+++ frontserver/src/main/java/reaper/frontserver/services/http/HttpService.java
@@ -1,7 +1,11 @@
 package reaper.frontserver.services.http;
 
 import com.squareup.okhttp.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import reaper.frontserver.exceptions.HttpExceptions;
+
+import java.io.IOException;
 
 public class HttpService
 {
@@ -47,39 +51,44 @@ public class HttpService
 
     public String get(String url) throws HttpExceptions.ServerError, HttpExceptions.NotFound, HttpExceptions.BadRequest
     {
+        Response response;
+
         try
         {
             Request request = new Request.Builder()
                     .url(url)
                     .build();
 
-            Response response = httpClient.newCall(request).execute();
-
-            int responseCode = response.code();
-            if (responseCode == 200)
-            {
-                return response.body().string();
-            }
-            else if (responseCode == 404)
-            {
-                throw new HttpExceptions.NotFound();
-            }
-            else if (responseCode == 400)
-            {
-                throw new HttpExceptions.BadRequest();
-            }
-            else
-            {
-                throw new HttpExceptions.ServerError();
-            }
-        }
-        catch (HttpExceptions.NotFound | HttpExceptions.BadRequest | HttpExceptions.ServerError e)
-        {
-            throw e;
+            response = httpClient.newCall(request).execute();
         }
         catch (Exception e)
         {
             throw new HttpExceptions.ServerError(e.getMessage());
+        }
+
+        int responseCode = response.code();
+        if (responseCode == 200)
+        {
+            try
+            {
+                return response.body().string();
+            }
+            catch (IOException e)
+            {
+                throw new HttpExceptions.ServerError("Unable to process response body");
+            }
+        }
+        else if (responseCode == 404)
+        {
+            throw new HttpExceptions.NotFound();
+        }
+        else if (responseCode == 400)
+        {
+            throw new HttpExceptions.BadRequest();
+        }
+        else
+        {
+            throw new HttpExceptions.ServerError();
         }
     }
 }
